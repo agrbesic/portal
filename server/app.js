757c7ba74cbdabb27cb623c5cb9b35e5aa3f9228
antegrbesic <a.grbesic@globalsoft.ba>
2022-12-11 16:04:37 +0100

    basic setup

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
+// server/index.js
+const path = require("path");
 const express = require("express");
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Html file");
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
-app.listen(2000);
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(9000);
