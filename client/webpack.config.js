f6e99bfb6b5e90f94ccc6447f7981f5056d1a36c
antegrbesic <a.grbesic@globalsoft.ba>
2022-12-29 15:28:44 +0100

    dockerize vue app

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+  devServer: {
+    port: 9000,
+  },
+};
--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
 // server/index.js
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
 });
 
 // All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3030;
